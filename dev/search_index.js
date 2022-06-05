fdeb3506f47b7dfca6a180d7cc6a4cfa62d6273a
Documenter.jl <documenter@juliadocs.github.io>
2022-06-05 14:49:11 +0000

    build based on e751a6d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = mintsOtter","category":"page"},{"location":"#mintsOtter","page":"Home","title":"mintsOtter","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for mintsOtter.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [mintsOtter]","category":"page"}]
+}
